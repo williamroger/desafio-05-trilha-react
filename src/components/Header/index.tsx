156fc61c6636b3adc3f27692e4e5d4eb16b44d42
William Roger <williamroger.frontend@gmail.com>
2022-04-09 02:10:59 -0300

    feitas configurações iniciais

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,10 @@
+import styles from './header.module.scss'
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src='/images/logo.svg' alt="Logo SpaceTraveling" />
+      </div>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,13 +24,15 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <h1>Home Posts</h1>
+  );
+}
 
 // export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+//   const prismic = getPrismicClient();
+//   const postsResponse = await prismic.query(TODO);
 
 //   // TODO
 // };
