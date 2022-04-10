32f40f245039cb8ae52c30d7f271ca0de410d2b7
William Roger <williamroger.frontend@gmail.com>
2022-04-09 22:25:46 -0300

    ajustes componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,16 @@
+import Link from 'next/link';
+
 import styles from './header.module.scss'
+
 export default function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src='/images/logo.svg' alt="Logo SpaceTraveling" />
+        <Link href="/">
+          <a>
+            <img src='/images/logo.svg' alt="logo" />
+          </a>
+        </Link>
       </div>
     </header>
   );
